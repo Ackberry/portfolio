00f2070496c7a677ab075eb569472f248222f23d
Ackberry <akbari@usf.edu>
2025-08-05 20:07:54 -0400

    updated resume

--- portfolio/src/app/page.tsx
+++ portfolio/src/app/page.tsx
@@ -31,7 +31,7 @@ export default function Home() {
             {/*Far right */}
             <div className="flex gap-4 flex-1 justify-end">
             <a
-            href="https://drive.google.com/file/d/1Y6rSVdh6YEw-hds-nGmSLECtbn0An6Ge/view?usp=sharing"
+            href="https://drive.google.com/file/d/1B-ZkmBp85jz1nFUOQfEeLT6kPIg5iRI5/view?usp=drive_link"
             target="_blank"
             rel="noopener noreferrer"
             aria-label="FileText"
