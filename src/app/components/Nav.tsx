10019b26d138cdced630de9e2b078b6c1dbdeb55
Deep Akbari <akbari@usf.edu>
2026-09-07 13:00:28 -0400

    Update resume URL in Nav component

--- src/app/components/Nav.tsx
+++ src/app/components/Nav.tsx
@@ -1,7 +1,7 @@
 import Link from 'next/link'
 import { FileText, Github, Linkedin, Mail } from 'lucide-react'
 
-export const RESUME_URL = 'https://drive.google.com/file/d/1R0LFPDq-tJ-R1ky_ZN_gWY4xNRgPbrUK/view?usp=sharing'
+export const RESUME_URL = 'https://drive.google.com/file/d/1pocPursy2Dh3G5eBsSdqiI_1aeMFp6Db/view?pli=1'
 export const GITHUB_URL = 'https://github.com/ackberry'
 export const LINKEDIN_URL = 'https://linkedin.com/in/deep-akbari'
 export const EMAIL = 'ackberrie@gmail.com'
